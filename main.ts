afb630cc7fd160d57043d1959162e3f02532d11f
Jmeow <j@jmeow.net>
2025-12-07 10:17:48 -0600

    Get list of subgroups (aka channels to bridge)

--- main.ts
+++ main.ts
@@ -22,7 +22,37 @@ async function sendMessage(group: string, message: string) {
         console.log(response)
         throw new Error("Message failed to send")
     }
-    return response
+    return response.response
 }
+async function getSubgroups(
+    group: string,
+): Promise<{ name: string; id: string }[]> {
+    const response = await (
+        await fetch(
+            "https://api.groupme.com/v3/groups/" + group + "/subgroups",
+            {
+                headers: {
+                    "X-Access-Token": Deno.env.get("GROUPME_TOKEN")!,
+                },
+            },
+        )
+    ).json()
+    if (response.meta.code != 200) {
+        console.log(response)
+        throw new Error("Failed to get subgroups")
+    }
+    return response.response.map((x: { topic: string; id: number }) => ({
+        name: x.topic,
+        id: x.id.toString(),
+    }))
+}
+const subgroups = (await getSubgroups(Deno.env.get("BASE_GROUP")!))
+    .filter((x) => x.name.startsWith("#"))
+    .map((x) => ({ ...x, name: x.name.slice(1) }))
 
-await sendMessage(Deno.env.get("BASE_GROUP")!, "Bot started")
+await sendMessage(
+    Deno.env.get("BASE_GROUP")!,
+    "Bot started, bridging channels:\n" +
+        subgroups.map((x) => "#" + x.name).join("\n") +
+        "\nto Discord.",
+)
